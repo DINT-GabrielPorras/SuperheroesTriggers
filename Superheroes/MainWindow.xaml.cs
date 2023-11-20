16120d1be3408547a3280e0b118eed5d9ecdee8e
GABRIEL PORRAS PÉREZ <gabporper@alu.edu.gva.es>
2023-11-20 11:57:34 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Superheroe> listaHeroes;
+        int contador = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            listaHeroes = Superheroe.GetSamples();
+
+            dockPanelPrincipal.DataContext = listaHeroes.FirstOrDefault();
+        }
+
+        public void FlechaDerecha_MouseLeftButtonDown(Object sender, EventArgs e)
+        {
+            if (contador > 0)
+                contador--;
+
+            dockPanelPrincipal.DataContext = listaHeroes[contador];
+
+            CambiaNumeros(contador);
+        }
+
+        public void FlechaIzquierda_MouseLeftButtonDown(Object sender, EventArgs e)
+        {
+            if(contador < 2)
+                contador++;
+
+            dockPanelPrincipal.DataContext = listaHeroes[contador];
+
+            CambiaNumeros(contador);
+        }
+
+        public void CambiaNumeros(int contador)
+        {
+            if (contador == 0)
+            {
+                numeros.Text = "1/3";              
+            }
+                
+            if (contador == 1)
+            {
+                numeros.Text = "2/3";              
+            }
+                
+            if (contador == 2)
+            {
+                numeros.Text = "3/3";                
+            }               
+        }
+    }
+}
